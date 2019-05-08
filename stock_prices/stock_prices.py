2cfbb3273e732ce77d96505a4c38dc202385152f
bluelogix <bluelogix010@gmail.com>
2019-05-08 18:02:01 -0400

    stock prices passes for all test except the -10

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 
 import argparse
-
+# profit = [10, 7, 5, 8, 11, 9]
 def find_max_profit(prices):
-  
+  max_price = 0
+  min_price = prices[0]
+  for i in prices:
+    min_price = min(min_price, i)
+    compare_price = i - min_price
+    max_price = max(max_price, compare_price)
+  return max_price
+# print(find_max_profit)
 
 
 if __name__ == '__main__':
@@ -12,4 +19,4 @@ if __name__ == '__main__':
   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
   args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers)) 
